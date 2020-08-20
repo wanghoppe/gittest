3f93c89ef029ee79a88ad56cb1c2a3e284216ae7
hoppe <wanghp000@gmail.com>
2020-08-20 20:08:40 +0800

    Revert "Update javascript.js"
    This reverts commit f53fc8d20758e4941e43d08dd57c7c6cc9d5ba2f.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 console.log('this is a javascript file')
+<<<<<<< HEAD
 function local(){console.log('local')}
 remote3 = 10
+||||||| f53fc8d... Update javascript.js
+funciton test(){console.log('this is just a test')}
+=======
+>>>>>>> parent of f53fc8d... Update javascript.js
