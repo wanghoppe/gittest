2378e0019320a5ce5b9730db500808f297334997
hoppe <wanghp000@gmail.com>
2020-08-20 20:09:24 +0800

    here1

--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
 console.log('this is a javascript file')
-<<<<<<< HEAD
 function local(){console.log('local')}
 remote3 = 10
-||||||| f53fc8d... Update javascript.js
-funciton test(){console.log('this is just a test')}
-=======
->>>>>>> parent of f53fc8d... Update javascript.js
+here1
